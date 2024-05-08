192e8a1a058b2d16a50203ebd9dd65300d109a56
friendlyPotat0 <friendlyPotat0@protonmail.com>
2024-05-08 13:05:03 -0500

    Idiomatic correctness, better structure, less hard coded

--- src/interface.rs
+++ src/interface.rs
@@ -5,18 +5,134 @@ use std::fs;
 use chrono::prelude::*;
 use rand::Rng;
 
-use neuroflow::activators::Type::Tanh;
 use neuroflow::data::DataSet;
 use neuroflow::FeedForward;
 use serde_json::Value;
 
-pub struct Interface {}
+pub struct Interface {
+    path: String,
+}
 
 impl Interface {
+    pub fn new (path: String) -> Interface {
+        Self { path }
+    }
+
+    pub fn train_neural_network(&mut self, samples: usize, topology: &[i32], activation_function: neuroflow::activators::Type, eta: f64, alpha: f64) {
+        // Get current UTC time
+        let utc: DateTime<Utc> = Utc::now();
+        let unix_timestamp = utc.timestamp();
+        let unix_timestamp_str = unix_timestamp.to_string();
+        // create neural network and specify topology
+        let mut nn = FeedForward::new(topology);
+        // define logfile for RMSE evolution
+        let filename = format!(
+            "resources/errors/rmse_{}_samples_{}.txt",
+            samples, unix_timestamp_str
+        );
+        nn.set_log_file(filename);
+        // define training data set
+        let mut data: DataSet = DataSet::new();
+        // read files from solar-radiation-database
+        let database_dir = format!("{}", self.path);
+        let files = fs::read_dir(database_dir).unwrap();
+        // Iterate over files
+        let mut i = 0;
+        for file in files {
+            if let Ok(file) = file {
+                let file_path = file.path();
+                if let Some(file_name) = file_path.file_name() {
+                    if let Some(file_name_str) = file_name.to_str() {
+                        i += 1;
+                        println!("Loading [{i}]: {}!", file_name_str);
+                        // Load JSON file
+                        let json_str = fs::read_to_string(file_path).unwrap();
+                        let json_data: Value = serde_json::from_str(&json_str).unwrap();
+                        // Extract latitude and longitude
+                        let latitude = json_data["inputs"]["location"]["latitude"]
+                            .as_f64()
+                            .unwrap();
+                        let longitude = json_data["inputs"]["location"]["longitude"]
+                            .as_f64()
+                            .unwrap();
+                        // Extract data from the "hourly" array
+                        if let Some(hourly_data) = json_data["outputs"]["hourly"].as_array() {
+                            for hourly_entry in hourly_data {
+                                let timestamp_str = hourly_entry["time"].as_str().unwrap();
+                                let power = hourly_entry["G(i)"].as_f64().unwrap();
+                                // Convert timestamp to Unix timestamp
+                                let unix_timestamp = Self::convert_to_unix_timestamp(timestamp_str);
+                                // Normalize latitude and longitude (you need to implement normalization)
+                                let latitude_norm = Self::normalize_latitude(latitude);
+                                let longitude_norm = Self::normalize_longitude(longitude);
+                                // Push data to training set
+                                data.push(
+                                    &[latitude_norm, longitude_norm, unix_timestamp as f64],
+                                    &[power],
+                                );
+                            }
+                        }
+                        if i >= samples {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        println!("Dataset filled!");
+        // define neural network architecture
+        nn.activation(activation_function)
+            .learning_rate(eta)
+            .momentum(alpha)
+            .train(&data);
+        println!("Training finished!");
+        // save model to disk
+        let filename = format!(
+            "resources/models/model_{}_samples_{}.flow",
+            samples, unix_timestamp_str
+        );
+        neuroflow::io::save(&mut nn, &filename).unwrap();
+        println!("Model saved to disk!");
+    }
+
+    fn normalize_latitude(latitude: f64) -> f64 {
+        // Latitude ranges from -90 to 90 degrees.
+        // Normalize it to the range [0, 1] or [-1, 1].
+        (latitude + 90.0) / 180.0 // Normalize to [0, 1]
+    }
+
+    fn normalize_longitude(longitude: f64) -> f64 {
+        // Longitude ranges from -180 to 180 degrees.
+        // Normalize it to the range [0, 1] or [-1, 1].
+        (longitude + 180.0) / 360.0 // Normalize to [0, 1]
+    }
+
+    fn convert_to_unix_timestamp(timestamp_str: &str) -> i64 {
+        // Parse the timestamp string
+        let datetime = NaiveDateTime::parse_from_str(timestamp_str, "%Y%m%d:%H%M").unwrap();
+        // Convert to UTC
+        let utc_datetime = Utc.from_utc_datetime(&datetime);
+        // Convert to Unix timestamp
+        let unix_timestamp = utc_datetime.timestamp();
+        return unix_timestamp;
+    }
+
+    /* pub fn print_result_from_loaded(path: &String, input: f64) {
+        let mut nn: FeedForward = neuroflow::io::load(path).unwrap();
+        let result = nn.calc(&[input])[0];
+        println!("{result}")
+    }
+
+    pub fn write_result_from_loaded_given_input_range(path: &String) {
+        let mut nn: FeedForward = neuroflow::io::load("resources/models/test.flow").unwrap();
+    } */
+
+    /* pub fn integrate_from_loaded_given_input_range_and_write() {} */
+
     pub fn train_test() {
         let mut nn = FeedForward::new(&[2, 2, 1]);
         let logfile = String::from("resources/errors/rmse_00.txt");
-        nn.set_logfile(&logfile);
+        nn.set_log_file(logfile);
         let mut data: DataSet = DataSet::new();
         let mut i = -3.0;
         let mut rng = rand::thread_rng();
@@ -26,7 +142,7 @@ impl Interface {
             data.push(&[a.into(), b.into()], &[(a ^ b).into()]);
             i += 0.05;
         }
-        nn.activation(Tanh)
+        nn.activation(neuroflow::activators::Type::Tanh)
             .learning_rate(0.1)
             .momentum(0.15)
             .train(&data);
@@ -48,112 +164,4 @@ impl Interface {
             i += 0.07;
         }
     }
-    pub fn train_neural_network(samples: usize) {
-        // Get current UTC time
-        let utc: DateTime<Utc> = Utc::now();
-        let unix_timestamp = utc.timestamp();
-        let unix_timestamp_str = unix_timestamp.to_string();
-
-        // create neural network and specify topology
-        let mut nn = FeedForward::new(&[3, 64, 32, 1]);
-
-        // define logfile for RMSE evolution
-        let filename = format!(
-            "resources/errors/rmse_{}_samples_{}.txt",
-            samples, unix_timestamp_str
-        );
-        nn.set_logfile(&filename);
-
-        // define training data set
-        let mut data: DataSet = DataSet::new();
-
-        // read files from solar-radiation-database
-        let database_dir = "resources/solar-radiation-database";
-        let files = fs::read_dir(database_dir).unwrap();
-
-        // Iterate over files
-        for file in files {
-            if let Ok(file) = file {
-                let file_path = file.path();
-                if let Some(file_name) = file_path.file_name() {
-                    if let Some(file_name_str) = file_name.to_str() {
-                        println!("Interpreting: {}!", file_name_str);
-                        // Load JSON file
-                        let json_str = fs::read_to_string(file_path).unwrap();
-                        let json_data: Value = serde_json::from_str(&json_str).unwrap();
-
-                        // Extract latitude and longitude
-                        let latitude = json_data["inputs"]["location"]["latitude"]
-                            .as_f64()
-                            .unwrap();
-                        let longitude = json_data["inputs"]["location"]["longitude"]
-                            .as_f64()
-                            .unwrap();
-
-                        // Extract data from the "hourly" array
-                        if let Some(hourly_data) = json_data["outputs"]["hourly"].as_array() {
-                            for hourly_entry in hourly_data {
-                                let timestamp_str = hourly_entry["time"].as_str().unwrap();
-                                let power = hourly_entry["G(i)"].as_f64().unwrap();
-
-                                // Convert timestamp to Unix timestamp
-                                let timestamp_unix = Self::convert_to_unix_timestamp(timestamp_str);
-
-                                // Normalize latitude and longitude (you need to implement normalization)
-                                let latitude_norm = Self::normalize_latitude(latitude);
-                                let longitude_norm = Self::normalize_longitude(longitude);
-
-                                // Push data to training set
-                                data.push(
-                                    &[latitude_norm, longitude_norm, timestamp_unix as f64],
-                                    &[power],
-                                );
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        // define neural network architecture
-        nn.activation(Tanh)
-            .learning_rate(0.1)
-            .momentum(0.15)
-            .train(&data);
-
-        // save model to disk
-        let filename = format!(
-            "resources/models/model_{}_samples_{}.flow",
-            samples, unix_timestamp_str
-        );
-        neuroflow::io::save(&mut nn, &filename).unwrap();
-    }
-
-    fn normalize_latitude(latitude: f64) -> f64 {
-        // Latitude ranges from -90 to 90 degrees.
-        // Normalize it to the range [0, 1] or [-1, 1].
-        (latitude + 90.0) / 180.0 // Normalize to [0, 1]
-    }
-
-    fn normalize_longitude(longitude: f64) -> f64 {
-        // Longitude ranges from -180 to 180 degrees.
-        // Normalize it to the range [0, 1] or [-1, 1].
-        (longitude + 180.0) / 360.0 // Normalize to [0, 1]
-    }
-
-    fn convert_to_unix_timestamp(timestamp_str: &str) -> i64 {
-        // Parse the timestamp string
-        let datetime = NaiveDateTime::parse_from_str(timestamp_str, "%Y%m%d:%H%M").unwrap();
-        // Convert to UTC
-        let utc_datetime = DateTime::<Utc>::from_utc(datetime, Utc);
-        // Convert to Unix timestamp
-        let unix_timestamp = utc_datetime.timestamp();
-        return unix_timestamp;
-    }
-
-    pub fn print_result_from_loaded() {}
-
-    pub fn write_result_from_loaded_given_input_range() {}
-
-    pub fn integrate_from_loaded_given_input_range_and_write() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,99 @@
 mod interface;
 
 use interface::Interface;
+use neuroflow::activators::Type;
 use std::{env, io};
 
 fn main() {
     env::set_var("RUST_BACKTRACE", "1");
-    println!("PVGIS NEURAL\n1. Train model\n2. Print power given timestamp\n3. Save to disk power profile (year)\n4. Save to disk calculated energy from timestamp range\n5. Quit");
-    let option = request_num_input(&"Enter option: ".to_string());
+    println!("PVGIS NEURAL");
+    println!("1. Train neural network");
+    println!("2. Predict power profile from time range (Unix Epoch)");
+    println!("3. Compute energy from profile");
+    println!("4. Quit");
+    let option = request_num_input("Enter option: ");
     match option {
         0 => Interface::train_test(),
         1 => {
-            let samples = request_num_input(&"Enter number of samples to feed: ".to_string());
-            Interface::train_neural_network(samples as usize)
-        },
-        2 => Interface::print_result_from_loaded(),
-        3 => Interface::write_result_from_loaded_given_input_range(),
-        4 => Interface::integrate_from_loaded_given_input_range_and_write(),
-        5 => println!("Bye!"),
+            let path = request_string_input("Enter path to database: ");
+            let mut interface = Interface::new(path);
+            let samples = request_num_input("Enter number of samples to feed: ");
+            let topology = request_int_slice_input("Enter topology: ");
+            let activation_function = request_activation_function();
+            let eta = request_double_input("Enter learning rate: ");
+            let alpha = request_double_input("Enter momentum: ");
+            interface.train_neural_network(samples as usize, &topology, activation_function, eta, alpha);
+        }
+        // 2 => Interface::print_result_from_loaded(),
+        // 3 => Interface::write_result_from_loaded_given_input_range(),
+        // 4 => Interface::integrate_from_loaded_given_input_range_and_write(),
+        4 => println!("Bye!"),
         _ => println!("Not a valid option. Try again"),
     }
 }
 
-fn request_num_input(message: &String) -> i32 {
-    let mut num = String::new();
+fn request_num_input(message: &str) -> i32 {
+    let mut input = String::new();
     print!("{}", message);
     io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout");
     io::stdin()
-        .read_line(&mut num)
+        .read_line(&mut input)
         .expect("Failed to read line");
-    let num: i32 = num.trim().parse().expect("Input must be an integer");
-    return num;
+    input.trim().parse().expect("Input must be an integer")
+}
+
+fn request_string_input(message: &str) -> String {
+    let mut input = String::new();
+    print!("{}", message);
+    io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input.trim().to_string()
+}
+
+fn request_double_input(message: &str) -> f64 {
+    let mut input = String::new();
+    print!("{}", message);
+    io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input.trim().parse().expect("Input must be an integer")
+}
+
+fn request_int_slice_input(message: &str) -> Vec<i32> {
+    let mut input = String::new();
+    print!("{}", message);
+    io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input
+        .trim()
+        .split_whitespace()
+        .filter_map(|s| s.parse().ok())
+        .collect()
+}
+
+fn request_activation_function() -> Type {
+    println!("Select an activation function:");
+    println!("1. Tanh");
+    println!("2. Relu");
+    println!("3. Sigmoid");
+    let mut input = String::new();
+    print!("Enter choice: ");
+    io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    match input.trim().parse::<usize>() {
+        Ok(1) => Type::Tanh,
+        Ok(2) => Type::Relu,
+        Ok(3) => Type::Sigmoid,
+        _ => {
+            println!("Invalid choice. Defaulting to Tanh");
+            Type::Tanh
+        }
+    }
 }
